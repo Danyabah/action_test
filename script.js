ecc492c081a2c2a13d35b7c0121b066e8204d452
Danyabah <tedrikyt881@gmail.com>
2024-02-16 14:52:16 +0300

    first

---
+++ script.js
@@ -0,0 +1,293 @@
+const data = [
+  {
+    number: 1,
+    video:
+      "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4?rlkey=33ijskx7iukufznmt03br671a&dl=0",
+    buttons: [
+      {
+        text: "Подойти",
+        video: 1,
+      },
+      {
+        text: "Подождать",
+        video: 2,
+      },
+      {
+        text: "Подойти",
+        video: 3, //??????
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 2,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "500милии",
+
+        video: 8,
+      },
+      {
+        text: "500микро",
+        video: 5,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 3,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "подойти",
+
+        video: 1,
+      },
+      {
+        text: "помахать",
+
+        video: 4,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 4,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "Поздороваться",
+
+        video: 1,
+      },
+      {
+        text: "Ждать",
+
+        video: 6,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 5,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 6,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "я случайно",
+
+        video: 7,
+      },
+      {
+        text: "отойти",
+
+        video: 6,
+      },
+      {
+        text: "отшутиться",
+
+        video: 19,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 7,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 8,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 9,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "мощный",
+
+        video: 10,
+      },
+      {
+        text: "стоять",
+
+        video: 9,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 10,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "отойти",
+        video: 6,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 11,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "предложить",
+        video: 11,
+      },
+      {
+        text: "какие качества?",
+        video: null,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 12,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "xiaomi",
+        video: 12,
+      },
+      {
+        text: "huawei",
+        video: 12,
+      },
+      {
+        text: "honor",
+        video: 13,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 13,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 14,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "предложить уши",
+        video: 14,
+      },
+      {
+        text: "предложить пб",
+        video: 15,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 15,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 16,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "1690",
+        video: 17,
+      },
+      {
+        text: "5590",
+        video: 16,
+      },
+    ],
+    isEnd: false,
+  },
+  {
+    number: 17,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 18,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [
+      {
+        text: "смартфон",
+        video: null,
+      },
+      {
+        text: "уши",
+        video: 14,
+      },
+    ],
+    isEnd: true,
+  },
+  {
+    number: 19,
+    video: "https://www.dropbox.com/scl/fi/cxw9jj1t7ya9w3swf3chy/forest.mp4",
+    buttons: [],
+    isEnd: true,
+  },
+  {
+    number: 20,
+    video: "https://kinescope.io/embed/e163EKUpCXAFsdPpahiL5S",
+    buttons: [
+      {
+        text: "мощный",
+        video: 10,
+      },
+      {
+        text: "стоять",
+        video: 9,
+      },
+    ],
+    isEnd: false,
+  },
+];
+
+const btnContainer = document.querySelector(".buttons__container");
+const video = document.querySelector(".video");
+const end = document.querySelector(".end");
+const wait = document.querySelector(".wait");
+const choice = document.querySelector(".choice");
+
+changeVideo(data[0]);
+
+function changeVideo(item) {
+  wait.classList.remove("hidden");
+  end.classList.add("hidden");
+  video.src = item.video;
+  video.play();
+  btnContainer.innerHTML = null;
+  choice.classList.add("hidden");
+  //   video.removeEventListener("ended");
+  if (!item.isEnd) {
+    video.onended = () => handleBtn(item);
+  } else {
+    end.classList.remove("hidden");
+  }
+}
+
+function handleBtn(item) {
+  wait.classList.add("hidden");
+  choice.classList.remove("hidden");
+  item.buttons.forEach((element) => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.innerHTML = element.text;
+    btn.onclick = () => changeVideo(data[element.video]);
+    btnContainer.append(btn);
+  });
+}
---
+++ test.js
@@ -0,0 +1,8 @@
+// const tag = document.createElement("script");
+// tag.src = "https://player.kinescope.io/v2.18.17/iframe.player.js";
+// const firstScriptTag = document.getElementsByTagName("script")[0];
+// firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+const player = document.getElementById("player");
+
+const video = player;
+console.log(video);
